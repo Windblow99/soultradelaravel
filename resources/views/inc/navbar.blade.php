e45889d9c90759fa8cd8902b718f934d16f80358
Eren <ongshaoan@gmail.com>
2021-02-28 17:02:33 +0800

    moved chat pos

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -40,11 +40,11 @@
         </li>
       @endif
     @else
+      <li class="nav-item">
+        <a class="nav-link" href="/chat">Chat</a>
+      </li>
       @can('medical-user')
       @if(Auth::user()->approved == "YES")
-        <li class="nav-item">
-          <a class="nav-link" href="/chat">Chat</a>
-        </li>
         <li class="nav-item dropdown">
           <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
             Requests
