b825c153acf28713b22a5b0cf4066a222e569cf5
Eren <ongshaoan@gmail.com>
2021-02-28 15:43:29 +0800

    Update navbar.blade.php

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -85,7 +85,7 @@
         </a>
 
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">
+          <a class="dropdown-item" href="#" style="cursor: default;">
             Balance: RM{{ Auth::user()->balance }}
           </a>
           
